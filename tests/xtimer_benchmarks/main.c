98d03b2539e709b78b056eae67c452e8e2937a6d
M Aiman Ismail <MuhammadAimanBin.Ismail@haw-hamburg.de>
2020-10-07 14:36:46 +0200

    fixup! fixup! fixup! add new testsuites for xtimer and ztimer benchmarks

--- tests/xtimer_benchmarks/main.c
+++ tests/xtimer_benchmarks/main.c
@@ -195,23 +195,23 @@ int timer_overhead_timer_cmd(int argc, char **argv)
     }
 
     unsigned timer_idx = -1;
-    if (strcmp(method, "set") == 0) {
-        if (strcmp(pos, "first") == 0) {
-            timer_idx = 0;
-        }
-        else if (strcmp(pos, "middle") == 0) {
-            timer_idx = (TEST_MAX_TIMERS / 2) - 1;
-        }
-        else if (strcmp(pos, "last") == 0) {
-            timer_idx = TEST_MAX_TIMERS - 1;
-        }
-        else {
-            goto error;
-        }
+    if (strcmp(pos, "first") == 0) {
+        timer_idx = 0;
+    }
+    else if (strcmp(pos, "middle") == 0) {
+        timer_idx = (TEST_MAX_TIMERS / 2) - 1;
+    }
+    else if (strcmp(pos, "last") == 0) {
+        timer_idx = TEST_MAX_TIMERS - 1;
+    }
+    else {
+        goto error;
+    }
 
+    if (strcmp(method, "set") == 0) {
         for (unsigned n = 0; n < TEST_REPEAT; ++n) {
             /* set all but the last timer */
-            for (unsigned i = 0; i < timer_idx - 1; ++i) {
+            for (unsigned i = 0; i < timer_idx; ++i) {
                 TIMER_SET(&test_timers[i], _delay(i));
             }
 
@@ -223,19 +223,6 @@ int timer_overhead_timer_cmd(int argc, char **argv)
         }
     }
     else if (strcmp(method, "remove") == 0) {
-        if (strcmp(pos, "first") == 0) {
-            timer_idx = 0;
-        }
-        else if (strcmp(pos, "middle") == 0) {
-            timer_idx = (TEST_MAX_TIMERS / 2) - 1;
-        }
-        else if (strcmp(pos, "last") == 0) {
-            timer_idx = TEST_MAX_TIMERS - 1;
-        }
-        else {
-            goto error;
-        }
-
         for (unsigned n = 0; n < TEST_REPEAT; ++n) {
             /* set timers until timer_idx */
             for (unsigned i = 0; i <= timer_idx; ++i) {
